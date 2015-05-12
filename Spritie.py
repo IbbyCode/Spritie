e7de5ba356efc2eb68d5e2b8704b06b821ab3965
IbbyCode <kallenrcunn@gmail.com>
2015-05-11 20:32:28 -0400

    Cleaned Up Code
    Converted function to class
    Added attributes dictionary, __init__() function, and kill() function to class

--- Spritie.py
+++ Spritie.py
@@ -1,16 +1,21 @@
-def sprite():
-    class Sprite():
-        x = 0
-        y = 0
-        xmomentum = 0
-        ymomentum = 0
-        slot = 0
-        def up(self, l = 1):
-            self.y += l
-        def down(self, l = 1):
-            self.y -= 1
-        def right(self, l = 1):
-            self.x += l
-        def left(self, l = 1):
-            self.x -= 1
-    return Sprite()
+class Sprite():
+    name = ''
+    x = 0
+    y = 0
+    xmomentum = 0
+    ymomentum = 0
+    slot = 0
+    attributes = {}
+    def __init__(self, name):
+        self.name = name
+        print('Sprite ',name,' created')
+    def up(self, l = 1):
+        self.y += l
+    def down(self, l = 1):
+        self.y -= l
+    def right(self, l = 1):
+        self.x += l
+    def left(self, l = 1):
+        self.x -= l
+    def kill(self):
+        del self
